6d83aaca67e4b81c75cc82c4bf1067c6f507868f
Alex Chipre <alechipre@gmail.com>
2022-12-23 12:22:01 -0500

    Proyecto Alura Encriptador V1.0

---
+++ index.js
@@ -0,0 +1,87 @@
+const textToEncrip = document.querySelector(".textarea-input");
+const botonEncrip = document.querySelector(".btn-enc");
+const textEncrip = document.querySelector(".textarea-output");
+
+const asideImg = document.querySelector('.doll');
+const asideMsjNo = document.querySelector('.msj-no-encontrado');
+const asideMsjCsj = document.querySelector('.msj-csj');
+const asideOut = document.querySelector('.textarea-output');
+const asideBtn = document.querySelector('.btn-copy');
+
+const infoIcon = document.querySelector('.ico-info');
+const infoText = document.querySelector('.text-info');
+
+const botonDesencrip = document.querySelector(".btn-desenc");
+
+// Funcion del boton para Encritar mensaje
+botonEncrip.addEventListener('click', function() {
+  // let textToConver = textToEncrip.value;
+  if(textToEncrip.value !== textToEncrip.value.toLowerCase() || textToEncrip.value !== textToEncrip.value.normalize("NFD")){
+    infoIcon.classList.add('rojo');
+    infoText.classList.add('rojo');
+    asideImg.classList.remove('disable');
+    asideMsjNo.classList.remove('disable');
+    asideMsjCsj.classList.remove('disable');
+    asideOut.classList.add('disable');
+    asideBtn.classList.add('disable');
+  } else {
+    textEncrip.value = textToEncrip.value.replace(/[aeiou]/g, function(match,pos,str){if(match == "a"){return "ai"} else if(match =="e"){return "enter"}else if(match =="i"){return "imes"}else if(match =="o"){return "ober"} else{ return "ufat"}});
+    infoIcon.classList.remove('rojo');
+    infoText.classList.remove('rojo');
+    if (textEncrip.value !== ""){
+      asideImg.classList.add('disable');
+      asideMsjNo.classList.add('disable');
+      asideMsjCsj.classList.add('disable');
+      asideOut.classList.remove('disable');
+      asideBtn.classList.remove('disable');
+    } else {
+      asideImg.classList.remove('disable');
+      asideMsjNo.classList.remove('disable');
+      asideMsjCsj.classList.remove('disable');
+      asideOut.classList.add('disable');
+      asideBtn.classList.add('disable');
+    }
+  }
+});
+botonDesencrip.addEventListener('click', function() {
+  if(textToEncrip.value !== textToEncrip.value.toLowerCase() || textToEncrip.value !== textToEncrip.value.normalize("NFD")){
+    infoIcon.classList.add('rojo');
+    infoText.classList.add('rojo');
+  } else {
+    textEncrip.value = textToEncrip.value.replace(/ai|enter|imes|ober|ufat/g, function(match){
+      if(match == "ai"){
+        return "a";
+      } else if(match =="enter"){
+        return "e";
+      } else if(match =="imes"){
+        return "i";
+      } else if(match =="ober"){
+        return "o";
+      } else if(match =="ufat"){
+        return "u";
+      }});
+    }
+    infoIcon.classList.remove('rojo');
+    infoText.classList.remove('rojo');
+    if (textEncrip.value !== ""){
+      asideImg.classList.add('disable');
+      asideMsjNo.classList.add('disable');
+      asideMsjCsj.classList.add('disable');
+      asideOut.classList.remove('disable');
+      asideBtn.classList.remove('disable');
+    } else {
+      asideImg.classList.remove('disable');
+      asideMsjNo.classList.remove('disable');
+      asideMsjCsj.classList.remove('disable');
+      asideOut.classList.add('disable');
+      asideBtn.classList.add('disable');
+    }
+  }
+);
+
+// Funcion del boton para Copiar mensaje
+asideBtn.addEventListener('click', function(){
+  navigator.clipboard.writeText(textEncrip.value);
+})
+
+
